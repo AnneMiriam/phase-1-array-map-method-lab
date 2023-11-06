8e9fd1285f17c1ea4e93d090e06ca8006d974bd6
Anne Hastings <anne.miriam@gmail.com>
2023-11-05 19:59:17 -0700

    complete lab

--- index.js
+++ index.js
@@ -11,6 +11,30 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   const splitElements = tutorials.map(word => word.split(' '));
+//   const capSplitWords = splitElements.map(words => words.map((word)=>word.charAt(0).toUpperCase()+word.slice(1)));
+//   // const rejoinElements = capSplitWords.map(sentences => sentences.join(' '));
+//   return tutorials = capSplitWords.map(sentences => sentences.join(' '));
+// }
 const titleCased = () => {
-  return tutorials
-}
+  const splitElements = tutorials.map(title => {
+    // * splitting each sentence string into individual word strings
+    const words = title.split(' ');
+    // * capitalizing the first word of each string
+    const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    // * rejoin each word string into it's original sentence string
+    return capitalizedWords.join(' ');
+  });
+  return splitElements;
+};
+const splitElements = titleCased();
+// !found 👆🏻 with help from chatGPT
+// const titleCased = () => {
+//   const words = tutorials.map(tutorial.split(' ')); // Split the string into words
+//   const capitalizedWords = words.map(function (word) {
+//     // Capitalize the first letter of each word
+//     return word.charAt(0).toUpperCase() + word.slice(1);
+//   });
+//   return capitalizedWords.join(' '); // Join the words back into a single string
+// };
